f5bb844d5755e5a45d0c99be5cbb48e27adaf394
Erica Viana <erica.viana@gmail.com>
2018-04-19 20:09:38 -0300

    Criação do teste deveRetornarVerdadeiroQuandoIniciarGerente

---
+++ src/main/java/com/Gerente.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Gerente {
+
+    private int performanceAnual;
+    private int MULTIPLICADOR = 3;
+
+    public Gerente(int performanceAnual) {
+        this.performanceAnual = performanceAnual;
+    }
+}
